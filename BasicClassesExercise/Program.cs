29a841c2cb3b40432bc7dc5116cbafdfd50185dc
yologan129 <logan128128@gmail.com>
2021-11-03 14:59:10 -0500

    made changes

---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>();
+        }
+    }
+}
